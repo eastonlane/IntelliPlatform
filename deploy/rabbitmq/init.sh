82de52110b51f5c53d24c354df01b6b59bf9eab9
Novemgron <novemgron@outlook.com>
2025-04-20 08:55:06 +0800

    feat: use ssh action

--- deploy/rabbitmq/init.sh
+++ deploy/rabbitmq/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Enabling MQTT plugins..."
-rabbitmq-plugins enable --offline rabbitmq_mqtt rabbitmq_web_mqtt
+rabbitmq-plugins enable --offline rabbitmq_mqtt
 
 # Start RabbitMQ in background
 echo "Starting RabbitMQ in background..."
@@ -11,19 +11,17 @@ rabbitmq-server -detached
 sleep 10
 
 # Read password from file
-RABBIT_PWD=$(cat /run/secrets/rabbitmq_pwd)
+RABBIT_USR=$(cat /run/secrets/rabbitmq_client_usr)
+RABBIT_PWD=$(cat /run/secrets/rabbitmq_client_pwd)
 
 # Optional: create vhost
-rabbitmqctl add_vhost my_vhost
+rabbitmqctl add_vhost iot
 
 # Add user with password from file
-rabbitmqctl add_user myuser "$RABBIT_PWD"
+rabbitmqctl add_user "$RABBIT_USR" "$RABBIT_PWD"
 
 # Set permissions for user (on vhost)
-rabbitmqctl set_permissions -p my_vhost myuser ".*" ".*" ".*"
-
-# Make user administrator (optional)
-rabbitmqctl set_user_tags myuser administrator
+rabbitmqctl set_permissions -p iot "$RABBIT_USR ".*" ".*" ".*"
 
 echo "Setup complete. Restarting RabbitMQ in foreground..."
 rabbitmqctl stop
