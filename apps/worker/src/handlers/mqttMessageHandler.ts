85a89dc383e82bf1c300cf2183ab5c525666b59a
Novemgron <novemgron@outlook.com>
2025-04-28 08:50:38 +0800

    feat: worker to handle mqtt event

--- apps/worker/src/handlers/mqttMessageHandler.ts
+++ apps/worker/src/handlers/mqttMessageHandler.ts
@@ -1,9 +1,11 @@
 import mqtt from "mqtt";
 import logger from "@/utils/logger";
+import { IMetricsService, MetricsService } from "@/services/metricsService";
 
 const topicPrefix = process.env.MQTT_SUBSCRIBE_TOPIC_PREVIX;
 const topicPrefixLevelCount =
   topicPrefix?.split("/").filter((x) => x.length > 0).length ?? 0;
+const metricsService: IMetricsService = new MetricsService();
 
 export const OnReceivingMessage: mqtt.OnMessageCallback = async (
   topic,
@@ -11,4 +13,36 @@ export const OnReceivingMessage: mqtt.OnMessageCallback = async (
 ) => {
   logger.info(`${topic}: ${message.toString()}`);
   const deviceId = topic.split("/").at(topicPrefixLevelCount);
+  if(!deviceId) {
+    return;
+  }
+  const payload: MqttMessagePayload = JSON.parse(message.toString());
+  metricsService.RecordMetrics({
+    payload: payload.props,
+    metaData: {
+      deviceId: deviceId,
+      tags: payload.tags.map(keyValueParser).filter((x) => !!x),
+      fields: payload.fields.map(keyValueParser).filter((x) => !!x),
+    },
+  });
 };
+
+function keyValueParser(str: string): { name: string; value: string } | null {
+  const subStr = str
+    .split(":")
+    .map((x) => x.trim())
+    .filter((x) => x.length > 0);
+  if (subStr.length != 2) {
+    return null;
+  }
+  return {
+    name: subStr[0],
+    value: subStr[1],
+  };
+}
+
+interface MqttMessagePayload {
+  props: string;
+  tags: string[];
+  fields: string[];
+}
--- apps/worker/src/services/metricsService.ts
+++ apps/worker/src/services/metricsService.ts
@@ -1,3 +1,70 @@
+import createDbConnection from "DAL";
+import {
+  metrics,
+  type MetricsDO,
+  type Tag,
+  type Field,
+} from "DAL/schema/metrics";
 
-export class MetricsService {
+export default interface MessageModel {
+  metaData: MessageMetaData;
+  payload: string;
+}
+
+interface MessageMetaData {
+  deviceId: string;
+  tags: Tag[];
+  fields: Field[];
+}
+
+export interface IMetricsService {
+  RecordMetrics: (msg: MessageModel) => Promise<void>;
+}
+
+export class MetricsService implements IMetricsService {
+  private db: ReturnType<typeof createDbConnection>;
+  constructor() {
+    this.db = createDbConnection(process.env.DATABASE_URL!);
+  }
+  public async RecordMetrics(msg: MessageModel) {
+    const properties = JSON.parse(msg.payload);
+    const metricsList: MetricsDO[] = [];
+    const time = new Date();
+
+    if (typeof properties == "object" && properties != null) {
+      for (const [key, value] of Object.entries(properties)) {
+        switch (typeof value) {
+          case "bigint":
+          case "number":
+            metricsList.push({
+              time: time,
+              deviceId: msg.metaData.deviceId,
+              name: key,
+              valueNumber: value.toString(),
+              valueBool: null,
+              tagList: msg.metaData.tags,
+              fieldList: msg.metaData.fields,
+            });
+            break;
+          case "boolean":
+            metricsList.push({
+              time: time,
+              deviceId: msg.metaData.deviceId,
+              name: key,
+              valueNumber: null,
+              valueBool: value,
+              tagList: msg.metaData.tags,
+              fieldList: msg.metaData.fields,
+            });
+          case "string":
+          default:
+            break;
+        }
+      }
+    }
+
+    if (metricsList.length > 0) {
+        await this.db.insert(metrics).values(metricsList)
+    }
+  }
 }
