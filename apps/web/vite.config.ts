ed4e0e26b6339575fa51404601f49933ae136c03
Novemgron <novemgron@outlook.com>
2025-05-04 22:21:49 +0800

    fix: locale parsing strategy config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
 		tailwindcss(),
 		sveltekit(),
 		paraglideVitePlugin({
+			strategy: ['url'],
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
 		})
