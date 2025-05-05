26d0bb1614dbf700cb1e4c2a9638a6524da8bf31
Novemgron <novemgron@outlook.com>
2025-05-05 11:42:00 +0800

    feat: logout

---
+++ apps/web/src/routes/(auth)/logout/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { localizeUrl } from '$lib/paraglide/runtime';
+	import { middleContainerClassString } from '$lib/sylte';
+	import { Alert } from 'flowbite-svelte';
+
+	$effect(() => {
+		setTimeout(() => {
+			goto(localizeUrl('/'), { invalidateAll: true });
+		}, 3000);
+	});
+</script>
+
+<div class={middleContainerClassString}>
+	<Alert>You are logout!</Alert>
+</div>
