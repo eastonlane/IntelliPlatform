2a0fdbbe1bd97b7b4df3deb66f85a59f330b0610
Novemgron <novemgron@outlook.com>
2025-05-05 12:19:07 +0800

    enhancement: layout distribution; fix: active URL does not work for home page in none base local

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -21,72 +21,75 @@
 		EditOutline,
 		LanguageOutline
 	} from 'flowbite-svelte-icons';
+	import { PruneSuffix } from '$lib/helper/url-helper';
 	import { getLocale, locales, localizeHref } from '$lib/paraglide/runtime';
 	let { children, data } = $props();
 
 	let currentLocale = $derived(getLocale());
-	let activeUrl = $state(page.url.pathname);
+	let activeUrl = $derived(page.url.pathname);
 </script>
 
 <Navbar>
-	<NavBrand href={localizeHref(page.url.pathname, { locale: currentLocale })}>
+	<NavBrand href={localizeHref('/', { locale: currentLocale })}>
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Iot</span>
 	</NavBrand>
 	<NavHamburger />
-	<NavUl {activeUrl}>
-		<NavLi href={localizeHref('/', { locale: currentLocale })}>{m.topBar_home()}</NavLi>
-		<NavLi href={localizeHref('/device', { locale: currentLocale })}>{m.topBar_devices()}</NavLi>
-		<NavLi href={localizeHref('/data', { locale: currentLocale })}>{m.topBar_data()}</NavLi>
+	<NavUl bind:activeUrl>
+		<NavLi href={PruneSuffix(localizeHref('/'), '/')}>{m.topBar_home()}</NavLi>
+		<NavLi href={localizeHref('/device')}>{m.topBar_devices()}</NavLi>
+		<NavLi href={localizeHref('/data')}>{m.topBar_data()}</NavLi>
 	</NavUl>
 
-	<Button
-		>{m['topBar.profileDropdownButton']()}
-		<ChevronDownOutline class="text-white dark:text-white" />
-	</Button>
-	<Dropdown class="w-30">
-		{#if data.user}
-			<DropdownItem
-				href={localizeHref('/logout', { locale: currentLocale })}
-				class="flex flex-row justify-between"
-			>
-				<EditOutline
-					class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-				/>
-				<sapn>{m['topBar.logout']()}</sapn>
-			</DropdownItem>
-			<DropdownDivider />
-		{:else}
-			<DropdownItem
-				href={localizeHref('/login', { locale: currentLocale })}
-				class="flex flex-row justify-between"
-			>
-				<ArrowRightToBracketOutline
-					class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-				/>
-				<span>{m['topBar.login']()}</span>
-			</DropdownItem>
-			<DropdownItem
-				href={localizeHref('/register', { locale: currentLocale })}
-				class="flex flex-row justify-between"
-			>
-				<ArrowUpFromBracketOutline
-					class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-				/>
-				<span>{m['topBar.signUp']()}</span>
-			</DropdownItem>
-		{/if}
-	</Dropdown>
+	<div class="flex flex-row justify-around gap-2 align-middle">
+		<Button
+			>{m['topBar.profileDropdownButton']()}
+			<ChevronDownOutline class="text-white dark:text-white" />
+		</Button>
+		<Dropdown class="w-30">
+			{#if data.user}
+				<DropdownItem
+					href={localizeHref('/logout', { locale: currentLocale })}
+					class="flex flex-row justify-between"
+				>
+					<EditOutline
+						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+					<sapn>{m['topBar.logout']()}</sapn>
+				</DropdownItem>
+				<DropdownDivider />
+			{:else}
+				<DropdownItem
+					href={localizeHref('/login', { locale: currentLocale })}
+					class="flex flex-row justify-between"
+				>
+					<ArrowRightToBracketOutline
+						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+					<span>{m['topBar.login']()}</span>
+				</DropdownItem>
+				<DropdownItem
+					href={localizeHref('/register', { locale: currentLocale })}
+					class="flex flex-row justify-between"
+				>
+					<ArrowUpFromBracketOutline
+						class="h-6 w-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
+					/>
+					<span>{m['topBar.signUp']()}</span>
+				</DropdownItem>
+			{/if}
+		</Dropdown>
 
-	<Button color="light"><LanguageOutline /></Button>
-	<Dropdown>
-		{#each locales as locale}
-			<DropdownItem rel="external" href={localizeHref(page.url.pathname, { locale: locale })}>
-				{new Intl.DisplayNames([locale], { type: 'language' }).of(locale)}
-			</DropdownItem>
-		{/each}
-	</Dropdown>
+		<Button color="light"><LanguageOutline /></Button>
+		<Dropdown>
+			{#each locales as locale}
+				<DropdownItem rel="external" href={localizeHref(page.url.pathname, { locale: locale })}>
+					{new Intl.DisplayNames([locale], { type: 'language' }).of(locale)}
+				</DropdownItem>
+			{/each}
+		</Dropdown>
 
-	<DarkMode />
+		<DarkMode />
+	</div>
 </Navbar>
 
 <div class="max-w-screen container w-screen p-12">
