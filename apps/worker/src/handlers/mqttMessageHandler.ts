277b31cf9472454566b2bf87dc1f7f6118dad9cd
Novemgron <novemgron@outlook.com>
2025-05-10 09:23:28 +0800

    fix: server terminate due to lack of exception handling; lack of validation;

--- apps/worker/src/handlers/mqttMessageHandler.ts
+++ apps/worker/src/handlers/mqttMessageHandler.ts
@@ -1,6 +1,10 @@
 import mqtt from "mqtt";
 import logger from "@worker/utils/logger";
-import { IMetricsService, MetricsService } from "@worker/services/metricsService";
+import {
+  IMetricsService,
+  MetricsService,
+} from "@worker/services/metricsService";
+import { validate } from "uuid";
 
 const topicPrefix = process.env.MQTT_SUBSCRIBE_TOPIC_PREVIX;
 const topicPrefixLevelCount =
@@ -11,20 +15,27 @@ export const OnReceivingMessage: mqtt.OnMessageCallback = async (
   topic,
   message
 ) => {
-  logger.info(`${topic}: ${message.toString()}`);
-  const deviceId = topic.split("/").at(topicPrefixLevelCount);
-  if(!deviceId) {
-    return;
+  try {
+    logger.info(`${topic}: ${message.toString()}`);
+    const deviceId = topic.split("/").at(topicPrefixLevelCount);
+    if (!deviceId || !validate(deviceId)) {
+      return;
+    }
+    const payload: MqttMessagePayload = JSON.parse(message.toString());
+    metricsService
+      .RecordMetrics({
+        payload: payload.props,
+        metaData: {
+          deviceId: deviceId,
+          tags: payload?.tags?.map(keyValueParser)?.filter((x) => !!x) ?? [],
+          fields:
+            payload?.fields?.map(keyValueParser)?.filter((x) => !!x) ?? [],
+        },
+      })
+      .catch((err) => logger.error(err, "Error during recording metrics --"));
+  } catch (err) {
+    logger.error(err, 'Error during handle mqtt message --')
   }
-  const payload: MqttMessagePayload = JSON.parse(message.toString());
-  metricsService.RecordMetrics({
-    payload: payload.props,
-    metaData: {
-      deviceId: deviceId,
-      tags: payload?.tags?.map(keyValueParser)?.filter((x) => !!x) ?? [],
-      fields: payload?.fields?.map(keyValueParser)?.filter((x) => !!x) ?? [],
-    },
-  });
 };
 
 function keyValueParser(str: string): { name: string; value: string } | null {
